29b1efb6072474ec76c6a1f4bf289dd63fdea34f
Kaan Dalkıran <kaan51dlkrn@gmail.com>
2024-07-08 11:23:43 +0300

    Initial Commit

---
+++ ShopPrime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShopPrime
+//
+//  Created by Trakya11 on 8.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
